36deef1ec5222cc2e6f3969c6ab3313a4b897cf7
Vasco Veloso <vveloso@gmail.com>
2013-02-12 14:17:35 +0000

    Implementação da listagem de funcionários na aplicação, URL /Employees

---
+++ obras-1213-web/obras-1213/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using obras_1213.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace obras_1213.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+            return View(Employee.AllEmployees());
+        }
+
+    }
+}
--- obras-1213-web/obras-1213/Models/Employee.cs
+++ obras-1213-web/obras-1213/Models/Employee.cs
@@ -17,6 +17,58 @@ namespace obras_1213.Models
         public string State { get; set; }
         public int ProfessionID { get; set; }
 
+        private string profession;
+        public string Profession
+        {
+            get
+            {
+                if (null != profession)
+                {
+                    return profession;
+                }
+                try
+                {
+                    using (SqlConnection conn = Db.Utils.NewConnection)
+                    {
+                        conn.Open();
+                        using (SqlCommand cmd = new SqlCommand(
+                            "select designacaoProf from Profissao where idProf = @prof", conn))
+                        {
+                            cmd.Parameters.AddWithValue("@prof", ProfessionID);
+                            using (SqlDataReader dr = cmd.ExecuteReader())
+                            {
+                                if (dr.Read())
+                                {
+                                    profession = dr.GetString(0);
+                                    return profession;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    throw new ModelException("Erro na base de dados.", ex.InnerException);
+                }
+                return "";
+            }
+        }
+
+        public Employee()
+        {
+        }
+
+        public Employee(int id, int nif, string name, int phone, string email, string state, int professionId)
+        {
+            ID = id;
+            NIF = nif;
+            Name = name;
+            Phone = phone;
+            Email = email;
+            State = state;
+            ProfessionID = professionId;
+        }
+
         public static Employee Find(int number)
         {
             try
@@ -33,15 +85,8 @@ namespace obras_1213.Models
                         {
                             if (dr.Read())
                             {
-                                Employee emp = new Employee();
-                                emp.ID = number;
-                                emp.NIF = dr.GetInt32(0);
-                                emp.Name = dr.GetString(1);
-                                emp.Phone = dr.GetInt32(2);
-                                emp.Email = dr.GetString(3);
-                                emp.State = dr.GetString(4);
-                                emp.ProfessionID = dr.GetInt32(5);
-                                return emp;
+                                return new Employee(number, dr.GetInt32(0), dr.GetString(1), dr.GetInt32(2),
+                                    dr.GetString(3), dr.GetString(4), dr.GetInt32(5));
                             }
                         }
                     }
@@ -52,6 +97,27 @@ namespace obras_1213.Models
                 throw new ModelException("Erro na base de dados.", ex.InnerException);
             }
             return null;
+        }
+
+        public static IEnumerable<Employee> AllEmployees()
+        {
+            using (SqlConnection conn = Db.Utils.NewConnection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(
+                    "select codFunc, nifFunc, nomeFunc, telefoneFunc, emailFunc, estadoFunc, profissao " +
+                    "from Funcionario", conn))
+                {
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            yield return new Employee(dr.GetInt32(0), dr.GetInt32(1), dr.GetString(2), dr.GetInt32(3),
+                                dr.GetString(4), dr.GetString(5), dr.GetInt32(6));
+                        }
+                    }
+                }
+            }
         }
     }
 }
